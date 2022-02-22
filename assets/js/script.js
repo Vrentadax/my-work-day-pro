5948b58bf269fd23d59f0cd6ab7acee512efdd6b
Vrentadax <vrentadax@outlook.com>
2022-02-22 08:15:15 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// check/display current day
+// standard timeblocks (9a-5p)
+// check if time is past/current/future and color code accordingly
+// clicking into event allows edit of events
+// save button saves
+// saved events persist in localstorage
+
