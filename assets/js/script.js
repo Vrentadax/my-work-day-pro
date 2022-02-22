a7c9c9c0edbf0e26d80e6c5a3aa71018e2ebbe9a
Vrentadax <vrentadax@outlook.com>
2022-02-22 14:55:33 -0700

    added function to display today's date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
-// check/display current day
 // standard timeblocks (9a-5p)
 // check if time is past/current/future and color code accordingly
 // clicking into event allows edit of events
 // save button saves
 // saved events persist in localstorage
 
+var tasks = {};
+
+// saves tasks to localStorage
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// load tasks
+var loadTasks = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    // create logic that will cycle through saved arrays
+};
+
+var displayToday = function () {
+    var today = moment().format("[Today is] dddd, MMMM Do");
+    console.log(today);
+    $("#currentDay").append(today);
+};
+
+displayToday();
