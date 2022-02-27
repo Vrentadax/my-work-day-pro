7f67b4f4527c5bfef071c7a397ba3fa296041289
Vrentadax <vrentadax@outlook.com>
2022-02-27 11:00:47 -0700

    began construction of task creation/load function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // save button saves
 // saved events persist in localstorage
 
-var tasks = {};
+var tasks = "";
 
 // saves tasks to localStorage
 var saveTasks = function () {
@@ -13,10 +13,14 @@ var saveTasks = function () {
 var loadTasks = function () {
     tasks = JSON.parse(localStorage.getItem("tasks"));
 
-    // create logic that will cycle through saved arrays
-    // should be first thing (minus displayToday) that runs
+    if(!tasks) {
+        tasks = ""; 
+    }
+
+    // need to cycle through tasks and append to correct time slot (pass to createtask function)
 };
 
+// displays today's date in header
 var displayToday = function () {
     var today = moment().format("[Today is] dddd, MMMM Do");
     console.log(today);
@@ -27,7 +31,7 @@ var displayToday = function () {
 var updateBlocks = function () {
     var time = moment().get("h");
     console.log(time);
-    for (var i=9; i<=17; i++) {
+    for (var i = 9; i <= 17; i++) {
         $(".container").find("t-" + i)
         if (time > i) {
             $(".t-" + i).addClass("past")
@@ -41,22 +45,45 @@ var updateBlocks = function () {
     }
 };
 
+// procedurally creates block items
 var createBlocks = function () {
-    for (var i=9; i <= 17; i++) {
+    for (var i = 9; i <= 17; i++) {
         var timeBlock = $("<div>").addClass("row time-block h-" + i);
         var time = $("<div>").addClass("hour col-1 d-inline pt-3").text(moment().hour(i).format("h A"));
-        var text = $("<div>").addClass("description col-10 d-inline pt-3 t-" + i).text("Sample");
+        var text = $("<div>").addClass("description col-10 d-inline pt-3 t-" + i);
         var save = $("<div>").addClass("saveBtn col-1 d-inline pt-3 s-" + i).text("Save");
         $(".container").append(timeBlock.append(time).append(text).append(save));
 
     }
     updateBlocks();
-}
+};
+
+var createTask = function() {
+    var taskP = $("<p>")
+    .text(taskText);
+
+    // need to create task (if any) and assign to correct block
+};
 
 displayToday();
 createBlocks();
 
+// event listener needs to listen to active click on block to allow p edit
+$(".description").on("click", "div", function () {
+    // get current text of p element
+    var text = $(this)
+        .text()
+        .trim();
+
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").val(text);
+    $(this).replaceWith(textInput);
+
+    // auto focus new element
+    textInput.trigger("focus");
+});
+
 // sets interval to check every 30 min for update to class
-setInterval(function() {
+setInterval(function () {
     updateBlocks()
-  }, ((1000*60)*30));
+}, ((1000 * 60) * 30));
